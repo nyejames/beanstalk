5ae1b9fa7dd7f9cda825d2c4dd4e94632956b358
nyejames <nyejamesmusic@gmail.com>
2026-02-21 13:01:33 +0000

    display summary

--- src/compiler_frontend/hir/hir_display.rs
+++ src/compiler_frontend/hir/hir_display.rs
@@ -1,1 +1,7 @@
-
+//! HIR Display
+//!
+//! Responsible for providing a way to get location and variable name information back from HIR.
+//!
+//! This will be used to help the rest of the HIR and borrow checker stages to create and return useful errors and warnings.
+//! (CompilerMessages)
+//! It will also enable printing out Hir structures for easy debugging also.
